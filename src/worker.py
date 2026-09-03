a6055d10d1308a1a2aa3a0c9f02de8b9e808422d
biraj/4444 <birahbiraj2@gmail.com>
2026-09-04 02:03:29 +0530

    Add Cloudflare Python Worker

---
+++ src/worker.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from workers import wsgi
+
+from public_site.app import create_app as create_public_app
+from admin_site.app import create_app as create_admin_app
+
+
+public_app = create_public_app()
+admin_app = create_admin_app()
+
+app = DispatcherMiddleware(
+    public_app,
+    {
+        "/admin": admin_app,
+    },
+)
+
+Default = wsgi.entrypoint(app)
